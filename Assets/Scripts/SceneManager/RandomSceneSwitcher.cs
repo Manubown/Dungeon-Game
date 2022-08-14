137a3d165abf56b7d7f60ef77ea743a000edd107
Manubown <manuel.gut030603@gmail.com>
2022-08-14 18:09:34 +0200

    All Scene Support Inventory + Scene Switch

--- Assets/Scripts/SceneManager/RandomSceneSwitcher.cs
+++ Assets/Scripts/SceneManager/RandomSceneSwitcher.cs
@@ -1,15 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class RandomSceneSwitcher : MonoBehaviour
 {
     public List<string> Scenes;
+    public KeyCode activationKey = KeyCode.F;
+    private bool playerTriggered = false;
 
-    public void OnTriggerEnter()
+    private void Update()
     {
-        int x = Random.Range(0, Scenes.Count - 1);
-        SceneManager.LoadScene(Scenes[x]);
+        if (Input.GetKeyDown(activationKey) && playerTriggered)
+        {
+            int x = Random.Range(0, Scenes.Count);
+
+            SceneManager.LoadScene(Scenes[0]);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerTriggered = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerTriggered = false;
+        }
     }
 }
